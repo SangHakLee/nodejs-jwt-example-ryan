06fecfea1005b7de27e9d52616a72e0b174d59c6
sanghaklee <code.ryan.lee@gmail.com>
2016-12-23 16:48:39 +0900

    app.js 추가

---
+++ app.js
@@ -0,0 +1,34 @@
+const express    = require('express');
+const bodyParser = require('body-parser');
+const morgan     = require('morgan');
+const mongoose   = require('mongoose');
+
+const config = require('./config');
+const port   = process.env.PORT || 3000; // 노드 실행시 포트 인자 사용 없으면 3000
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+app.set('jwt-secret', config.secret); // config.js 파일의 secret을 사용
+
+app.get('/', (req, res) => {
+    res.send('JWT index');
+});
+
+app.listen(port, () => {
+    console.log(`Express running on ${port}`);
+});
+
+
+// mongodb
+mongoose.connect(config.mongodbUri);
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+    console.log('mongoose is running');
+});
